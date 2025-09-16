ac7ff939e7ffdc29fd7921afba44ae8babb29b57
JuanSassi <juan.sassi@mi.unc.edu.ar>
2025-09-15 21:24:24 -0300

    Constants

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -5,26 +5,33 @@
  * @author Sassi Juan Ignacio
  */
 class Constants {
-    private Constants() {} // Avoidance of instantiation
-
-    /** Number of places in the Petri net */
+    /** Prevents instantiation */
+    private Constants() {}
+    
+    /** Number of places in the Petri Net */
     public static final int NUM_P = 15;
-    /** Number of transitions in the Petri net */
+
+    /** Number of transitions in the Petri Net */
     public static final int NUM_T = 12;
-
-    /** Multiple runs must be performed with 186 invariants completed for each run */
+    
+    /** Maximum number of invariants to complete before ending simulation */
     public static final int MAX_INVARIANTS = 186;
+    
+    /** P0: Idle place corresponding to the agency's client input buffer */
+    public static final int CLIENT = 5;
 
-    /** Idle place corresponding to the client input buffer */
-    public static final int CLIENT = 5;         // P0
-    /** Shared resource: door access control */
-    public static final int DOOR = 1;           // P1
-    /** Number of available assistants */
-    public static final int ASSISTANTS = 5;     // P4
-    /** Shared resource: manager1 availability */
-    public static final int AGENT_P6 = 1;       // P6
-    /** Shared resource: manager2 availability */
-    public static final int AGENT_P7 = 1;       // P7
-    /** Shared resource: travel agent availability */
-    public static final int AGENT_P10 = 1;      // P10
+    /** P1: Door access control */
+    public static final int DOOR = 1;
+
+    /** P4: Number of available assistants */
+    public static final int ASSISTANTS = 5;
+
+    /** P6: Top reservation agent */
+    public static final int AGENT_P6 = 1;
+
+    /** P7: Bottom reservation agent */
+    public static final int AGENT_P7 = 1;
+
+    /** P10: Approval/Denial reservation agent */
+    public static final int AGENT_P10 = 1;
 }
